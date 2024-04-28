27dc26a1908b95f1e33542312b886565ad9829f2
Reza Rajan <28660160+rezarajan@users.noreply.github.com>
2024-04-27 20:33:29 -0400

    (update) include task dependencies and task groups in final dag
    Signed-off-by: Reza <28660160+rezarajan@users.noreply.github.com>

--- projects/4 - Data Pipelines/dags/final_project.py
+++ projects/4 - Data Pipelines/dags/final_project.py
@@ -1,7 +1,6 @@
 import pendulum
 
-from datetime import datetime, timedelta
-from airflow.decorators import dag
+from airflow.decorators import dag, task_group
 from airflow.operators.dummy import DummyOperator
 from operators import (StageToRedshiftOperator, LoadFactOperator,
                        LoadDimensionOperator, DataQualityOperator)
@@ -26,66 +25,83 @@ def final_project():
 
     # Stage the event log data to Redshift.
     # YYYY/MM partitioning is enable via templating on the s3_key
-    stage_events_to_redshift = StageToRedshiftOperator(
-        task_id='stage_events',
-        redshift_conn_id='redshift',
-        aws_credentials_id='aws_credentials',
-        table='staging_events',
-        s3_bucket='aws-dend-airflow/project-4',
-        s3_key='log-data/{{ execution_date.strftime("%Y/%m") }}',
-        json='log_json_path.json'
-    )
+    @task_group
+    def staging_group():
+        """Staging tasks"""
+        stage_events_to_redshift = StageToRedshiftOperator(
+            task_id='stage_events',
+            redshift_conn_id='redshift',
+            aws_credentials_id='aws_credentials',
+            table='staging_events',
+            s3_bucket='aws-dend-airflow/project-4',
+            s3_key='log-data/{{ execution_date.strftime("%Y/%m") }}',
+            json='log_json_path.json'
+        )
 
-    stage_songs_to_redshift = StageToRedshiftOperator(
-        task_id='stage_songs',
-        redshift_conn_id='redshift',
-        aws_credentials_id='aws_credentials',
-        table='staging_songs',
-        s3_bucket='aws-dend-airflow/project-4',
-        s3_key='song-data',
-    )
+        stage_songs_to_redshift = StageToRedshiftOperator(
+            task_id='stage_songs',
+            redshift_conn_id='redshift',
+            aws_credentials_id='aws_credentials',
+            table='staging_songs',
+            s3_bucket='aws-dend-airflow/project-4',
+            s3_key='song-data',
+        )
 
-    load_songplays_table = LoadFactOperator(
-        task_id='load_songplays_fact_table',
-        redshift_conn_id='redshift',
-        table='songplays',
-        sql=SqlQueries.songplay_table_insert
-    )
+    @task_group
+    def load_facts():
+        """Load fact tables"""
+        load_songplays_table = LoadFactOperator(
+            task_id='load_songplays_fact_table',
+            redshift_conn_id='redshift',
+            table='songplays',
+            sql=SqlQueries.songplay_table_insert
+        )
 
-    load_user_dimension_table = LoadDimensionOperator(
-        task_id='load_user_dim_table',
-        redshift_conn_id='redshift',
-        table='users',
-        sql=SqlQueries.user_table_insert
-    )
+    @task_group
+    def load_dimensions():
+        "Load dimension tables"
+        load_user_dimension_table = LoadDimensionOperator(
+            task_id='load_user_dim_table',
+            redshift_conn_id='redshift',
+            table='users',
+            sql=SqlQueries.user_table_insert
+        )
 
-    load_song_dimension_table = LoadDimensionOperator(
-        task_id='load_song_dim_table',
-        redshift_conn_id='redshift',
-        table='songs',
-        sql=SqlQueries.song_table_insert
-    )
+        load_song_dimension_table = LoadDimensionOperator(
+            task_id='load_song_dim_table',
+            redshift_conn_id='redshift',
+            table='songs',
+            sql=SqlQueries.song_table_insert
+        )
 
-    load_artist_dimension_table = LoadDimensionOperator(
-        task_id='load_artist_dim_table',
-        redshift_conn_id='redshift',
-        table='artists',
-        sql=SqlQueries.artist_table_insert
-    )
+        load_artist_dimension_table = LoadDimensionOperator(
+            task_id='load_artist_dim_table',
+            redshift_conn_id='redshift',
+            table='artists',
+            sql=SqlQueries.artist_table_insert
+        )
 
-    load_time_dimension_table = LoadDimensionOperator(
-        task_id='load_time_dim_table',
-        redshift_conn_id='redshift',
-        table='time',
-        sql=SqlQueries.time_table_insert
-    )
+        load_time_dimension_table = LoadDimensionOperator(
+            task_id='load_time_dim_table',
+            redshift_conn_id='redshift',
+            table='time',
+            sql=SqlQueries.time_table_insert
+        )
 
-    run_quality_checks = DataQualityOperator(
-        task_id='run_data_quality_checks',
-        redshift_conn_id='redshift_test',
-        sql=TestSuite.row_count_sql.format(table='songs'),
-        test_function=TestSuite.row_count_test
-    )
+    @task_group
+    def run_tests():
+        """Run all data quality checks"""
+        run_quality_checks = DataQualityOperator(
+            task_id='run_data_quality_checks',
+            redshift_conn_id='redshift',
+            sql=TestSuite.row_count_sql.format(table='songs'),
+            test_function=TestSuite.row_count_test
+        )
+    end_operator = DummyOperator(task_id='End execution')
+
+    start_operator >> staging_group() >> load_facts(
+    ) >> load_dimensions() >> run_tests() >> end_operator
 
 
 final_project_dag = final_project()
+
